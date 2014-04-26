6c17bb3792381d7e8afaa78234306b2f3d3a7864
CJ Ponti <cjponti@gmail.com>
2014-04-26 09:35:21 +0100

    Fix gulpfile, bower, and directory structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,26 @@
 var gulp = require('gulp');
-// var less = require('gulp-less');
-// var concat = require('gulp-concat');
+var less = require('gulp-less');
+var jshint = require('gulp-jshint');
+var stylish = require('jshint-stylish');
 var livereload = require('gulp-livereload');
 var gutil = require('gulp-util');
 var staticServer = require('node-static');
 var http = require('http');
 
-gulp.task('server', function(next) {
+var jsHint = function() {
+	gulp.src('assets/js/**/*.js')
+		.pipe(jshint())
+		.pipe(jshint.reporter(stylish))
+		.pipe(gulp.dest('public/js'));
+};
+
+var lessLint = function() {
+	return gulp.src('assets/less/main.less')
+		.pipe(less())
+		.pipe(gulp.dest('public/css'));
+};
+
+var staticServer = function(next) {
 	var staticS = require('node-static');
 	var server = new staticServer.Server('./');
 	var port = 3000;
@@ -19,18 +33,14 @@ gulp.task('server', function(next) {
 		gutil.log('Server listening on port: ' + gutil.colors.magenta(port));
 		next();
 	});
+};
+
+gulp.task('server', staticServer);
+gulp.task('jshint', jsHint);
+gulp.task('less', lessLint);
+
+gulp.task('watch', function() {
+	gulp.watch('assets/**/*.less', [ 'less' ]);
 });
-
-// gulp.task('less', function() {
-// 	gulp.src('src/boiler.less')
-// 		.pipe(less())
-// 		.pipe(gulp.dest('dist'))
-// });
-
-// gulp.task('watch', function() {
-// 	gulp.watch('src/**/*.less', ['less']);
-// });
-
-// gulp.task('build', [ 'less', 'concat' ]);
 
 gulp.task('default', [ 'server' ]);
