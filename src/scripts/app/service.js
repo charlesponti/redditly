775b725ddeddce5c780f1967b10d431fc4f79927
CJ Ponti <cjponti@gmail.com>
2014-12-01 15:44:23 +0000

    Add documentation for service.search

--- src/scripts/app/service.js
+++ src/scripts/app/service.js
@@ -1,7 +1,14 @@
 'use strict';
 
+var $ = require('jquery');
+
 var service = {
-  search: function(query, box) {
+  /**
+   * Search Reddit for sub-reddit matching query
+   * @param  {string} query
+   * @return {$.Promise}
+   */
+  search: function(query) {
     return $.get("http://www.reddit.com/r/"+query+'/.json');
   }
 };
