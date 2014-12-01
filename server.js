adece28fa794c2d3ad29031a4f0890b3dead091a
CJ Ponti <cjponti@gmail.com>
2014-12-01 23:18:08 +0000

    Remove consolidate

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ var http = require('http');
 var path = require('path');
 var swig = require('swig');
 var express = require('express');
-var engines = require('consolidate');
 
 // Create app
 var app = express();
