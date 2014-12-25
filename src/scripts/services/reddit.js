d6112dd890b337f2efbcfad225eab1f59896e743
CJ Ponti <cjponti@gmail.com>
2014-12-25 10:44:04 +0000

    Use $resource for Reddit API

--- src/scripts/services/reddit.js
+++ src/scripts/services/reddit.js
@@ -1,28 +1,8 @@
 'use strict';
 
 
-module.exports = function($http, $q) {
-  return {
-    /**
-    * Search Reddit for sub-reddit matching query
-    * @param  {string} query
-    * @return {$.Promise}
-    */
-    search: function(query) {
-      var deferred = new $q.defer();
-
-      if (query) {
-        $http
-          .get('http://www.reddit.com/r/'+query+'.json')
-          .success(function(response) {
-            return deferred.resolve(response);
-          })
-          .error(function(response) {
-            return deferred.reject(response);
-          });
-      }
-
-      return deferred.promise;
-    }
-  };
+module.exports = function($http, $q, $resource) {
+  return $resource('http://www.reddit.com/r/:query.json', {
+    search: { method: 'GET' }
+  });
 };
