f8a7750063049933d39934dce259a4bb8918597d
CJ Ponti <cjponti@gmail.com>
2014-12-25 10:05:55 +0000

    Remove React files

--- src/scripts/app/dispatcher.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var _ = require('lodash');
-var Backbone = require('backbone');
-
-// Extend Backbone.Events to create applicaton event dispatcher
-var AppDispatcher = _.extend({}, Backbone.Events);
-
-module.exports = AppDispatcher;
