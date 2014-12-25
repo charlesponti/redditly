300353c30f45a36570c3908322062fb90be5ecee
CJ Ponti <cjponti@gmail.com>
2014-12-25 13:16:53 +0000

    Add angular-animate

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,10 +4,11 @@
 require('angular');
 require('angular-route');
 require('angular-resource');
+require('angular-animate');
 
 var angular = window.angular;
 
-angular.module('Redditly', ['ngRoute', 'ngResource'])
+angular.module('Redditly', ['ngRoute', 'ngResource', 'ngAnimate'])
   .config(require('./router'))
   .factory('Reddit', require('./services/reddit'))
   .controller('HomeCtrl', require('./controllers/home'));
