6958b4136ed775a14a1522ee24ab2ced6cdd2da6
CJ Ponti <cjponti@gmail.com>
2014-12-29 12:58:54 +0000

    Add tests for .onSearchSuccess()

--- test/spec/controllers/home-spec.js
+++ test/spec/controllers/home-spec.js
@@ -4,7 +4,7 @@ describe('Controllers: HomeCtrl', function() {
   'use strict';
 
   var $scope;
-
+  var $ = require('jquery');
   require('../spec-helper.js');
 
   beforeEach(function() {
@@ -41,6 +41,39 @@ describe('Controllers: HomeCtrl', function() {
       $scope.error = true;
       $scope.toggleError();
       expect($scope.error).toEqual(false);
+    });
+  });
+
+  describe('.onSearchSuccess()', function() {
+    var response = {
+      data: {
+        children: [
+          { data: { title: 'foo' } },
+          { data: { title: 'bar' } },
+        ]
+      }
+    };
+    it('should remove .out', function() {
+      spyOn($.fn, 'removeClass');
+      $scope.onSearchSuccess(response);
+      expect($.fn.removeClass).toHaveBeenCalledWith('out');
+    });
+    it('should empty $scope.query', function() {
+      $scope.query = "foo";
+      $scope.onSearchSuccess(response);
+      expect($scope.query).toEqual('');
+    });
+    it('should set $scope.error to false', function() {
+      $scope.error = true;
+      $scope.onSearchSuccess(response);
+      expect($scope.error).toEqual(false);
+    });
+    it('should populate $scope.links', function() {
+      $scope.onSearchSuccess(response);
+      expect($scope.links).toEqual([
+        {title: 'foo'},
+        {title: 'bar'}
+      ]);
     });
   });
 
