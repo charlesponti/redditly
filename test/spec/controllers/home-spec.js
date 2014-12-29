58d2cb6140efcd58800ba888d0bf1c14b4a94bf3
CJ Ponti <cjponti@gmail.com>
2014-12-29 13:22:16 +0000

    Add tests for onSearchFail and onSubmit

--- test/spec/controllers/home-spec.js
+++ test/spec/controllers/home-spec.js
@@ -3,21 +3,37 @@
 describe('Controllers: HomeCtrl', function() {
   'use strict';
 
-  var $scope;
+  var $scope, $reddit, $q, response;
   var $ = require('jquery');
   require('../spec-helper.js');
 
-  beforeEach(function() {
+  beforeEach(function(done) {
+    response = {
+      data: {
+        children: [
+          { data: { title: 'foo' } },
+          { data: { title: 'bar' } },
+        ]
+      }
+    };
+
     // mock the controller
-    angular.mock.inject(function($rootScope, $controller) {
+    angular.mock.inject(function($rootScope, $controller, $injector) {
+      $q = $injector.get('$q');
+      $reddit = $injector.get('Reddit');
       $scope = $controller('HomeCtrl', {
-        $scope: $rootScope.$new()
+        $scope: $rootScope.$new(),
+        Reddit: $reddit
       });
+      done();
     });
   });
 
   afterEach(function() {
-    $scope = undefined;
+    $q =
+    $scope =
+    $reddit =
+    response = undefined;
   });
 
   it('should have title defined', function() {
@@ -45,14 +61,6 @@ describe('Controllers: HomeCtrl', function() {
   });
 
   describe('.onSearchSuccess()', function() {
-    var response = {
-      data: {
-        children: [
-          { data: { title: 'foo' } },
-          { data: { title: 'bar' } },
-        ]
-      }
-    };
     it('should remove .out', function() {
       spyOn($.fn, 'removeClass');
       $scope.onSearchSuccess(response);
@@ -77,4 +85,37 @@ describe('Controllers: HomeCtrl', function() {
     });
   });
 
+  describe('.onSearchFail()', function() {
+    it('should remove .out', function() {
+      spyOn($.fn, 'removeClass');
+      $scope.onSearchFail(response);
+      expect($.fn.removeClass).toHaveBeenCalledWith('out');
+    });
+    it('should empty $scope.links', function() {
+      $scope.links = [
+        {title: 'foo'},
+        {title: 'bar'}
+      ];
+      $scope.onSearchFail(response);
+      expect($scope.links).toEqual([]);
+    });
+    it('should set $scope.error to true', function() {
+      $scope.error = false;
+      $scope.onSearchFail(response);
+      expect($scope.error).toEqual(true);
+    });
+  });
+
+  describe('.onSubmit()', function() {
+    it('should call Reddit.get() and set $scope.currentSearch', function() {
+      var promise = $q.defer().promise;
+      spyOn(promise, 'then');
+      spyOn(promise, 'catch');
+      $scope.query = "New York";
+      spyOn($reddit, 'get').and.returnValue({ $promise:  promise });
+      $scope.onSubmit();
+      expect($reddit.get).toHaveBeenCalledWith({ query: 'NewYork' });
+      expect($scope.currentSearch).toEqual('New York');
+    });
+  });
 });
